1f81b723a2f488f3bf6feb81afdd167ba2608ef2
Sydney Santos <139241761+Syddevv@users.noreply.github.com>
2024-12-28 11:42:42 +0800

    Add files via upload
    uploaded files

---
+++ diceGuessing.js
@@ -0,0 +1,25 @@
+const guessInput = document.getElementById("numberDropdown");
+const rollButton = document.getElementById("roll-button");
+const scoreDisplay = document.getElementById("score");
+const playerGuessResult = document.getElementById("player-guess-result");
+const diceResult = document.getElementById("number-result");
+const imageContainer = document.getElementById("image-container");
+let score = 0;
+
+rollButton.addEventListener("click", () => {
+  let randomDice = Math.floor(Math.random(diceResult) * 6) + 1;
+  let guess = guessInput.value;
+  guess = Number(guess);
+
+  if (guess === randomDice) {
+    playerGuessResult.textContent = "YOU WIN!";
+    score++;
+  } else {
+    playerGuessResult.textContent = "YOU LOSE!";
+  }
+
+  diceResult.textContent = `Result: ${randomDice}`;
+  scoreDisplay.textContent = `Score: ${score}`;
+
+  imageContainer.innerHTML = `<img id="image-result" src="images/dice/${randomDice}.png" alt="" />`;
+});
